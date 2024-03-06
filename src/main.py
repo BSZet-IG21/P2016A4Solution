7e4bc1ec2f67c7e479c0c5cdbd2f96458dc86bcc
CHr <christoph.trischler@t-online.de>
2024-03-06 13:44:11 +0100

    init

---
+++ src/main.py
@@ -0,0 +1,137 @@
+import sys
+from datetime import date
+from typing import List 
+from PySide6.QtWidgets import QApplication, QMainWindow, QFileDialog, QTableWidgetItem 
+
+from ui_mainwindow import Ui_MainWindow
+
+
+class QTableWidgetItemDate(QTableWidgetItem):   
+    def __init__(self, d: date) -> None:
+        self.date = d 
+        super().__init__(str(d)) 
+
+    def __lt__(self, other) -> bool:
+        return self.date < other.date
+
+
+def format_date(s: str) -> date: 
+    year = int(s[0:4])
+    month = int(s[4:6])
+    day = int(s[6:8])
+    return date(year, month, day)
+
+def format_time(s: str) -> str:
+    hours = s[0:2]
+    minutes = s[2:4]
+    return f"{hours}:{minutes}"
+
+def parse_vevent(lines: List[str]) -> List[str | date]:
+    #summary
+    line = lines.pop(0)
+    [_, value] = line.split(":", 1)
+    title = value
+    start = lines.pop(0)
+    end = lines.pop(0)
+    [key_start, value_start] = start.split(":", 1)
+    [_, value_end] = end.split(":", 1)
+    start_date =""
+    start_time = ""
+    end_date = ""
+    end_time = ""
+    if key_start.split("=",1)[1] == "DATE":
+        start_date = format_date(value_start)
+        end_date = format_date(value_end)
+    else: 
+        [date, time] = value_start.split("T", 1)
+        start_date = format_date(date)
+        start_time = format_time(time)    
+        [date, time] = value_end.split("T", 1)
+        end_date = format_date(date)
+        end_time = format_time(time)
+    #location or description 
+    line = lines.pop(0)
+    [key, value] = line.split(":", 1)
+    ort = ""
+    if key == "LOCATION": 
+        ort = value
+        line = lines.pop(0)
+        [key, value] = line.split(":", 1)
+    beschreibung = value
+    return [
+        start_date,
+        start_time,
+        end_date,
+        end_time,
+        ort,
+        title,
+        beschreibung 
+    ]
+    
+
+class MainWindow(QMainWindow, Ui_MainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+        self.pb_open.clicked.connect(self.read_file)
+        self.pb_fertig.setVisible(False)
+        self.pb_new_event.clicked.connect(self.insert_row_4_event)
+        self.pb_fertig.clicked.connect(self.finish_event)
+
+    def read_file(self):
+        self.tableWidget.setRowCount(0)
+        (file_name, _) = QFileDialog.getOpenFileName(self)
+        if file_name is None:
+            print("no file selected")
+            return
+ 
+        with open(file_name, "r") as f:
+            text = f.read() 
+            lines = text.split("\n")
+            line = lines.pop(0)
+            while line is not None: 
+                while line != "BEGIN:VEVENT":
+                    if len(lines) == 0: 
+                        return 
+                    line = lines.pop(0)
+                event = parse_vevent(lines)
+                line = lines.pop(0)
+                self.display_event(event)
+
+
+    def display_event(self, event: List[str | date]):
+        t = self.tableWidget
+        t.setRowCount(t.rowCount() + 1)
+        for column, el in enumerate(event):
+            if isinstance(el, str):
+                t.setItem(t.rowCount() - 1, column, QTableWidgetItem(el))
+            else: 
+                t.setItem(t.rowCount() -1, column, QTableWidgetItemDate(el))
+
+
+    def insert_row_4_event(self):
+        self.tableWidget.setRowCount(self.tableWidget.rowCount() + 1)
+        self.pb_fertig.setVisible(True)
+        self.pb_new_event.setVisible(False)
+
+
+    def finish_event(self):
+        t = self.tableWidget
+        row = t.rowCount() -1 
+        s = t.item(row, 0).text()
+        #format dd.mm.yyyy
+        day = int(s[0:2])
+        month = int(s[3:5])
+        year = int(s[6:10])
+        item = QTableWidgetItemDate(date(year, month, day)) 
+        t.setItem(row, 0, item)
+        self.tableWidget.sortItems(0)
+        self.pb_fertig.setVisible(False)
+        self.pb_new_event.setVisible(True)
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    sys.exit(app.exec())
---
+++ src/ui_mainwindow.py
@@ -0,0 +1,92 @@
+# -*- coding: utf-8 -*-
+
+################################################################################
+## Form generated from reading UI file 'mainwindow.ui'
+##
+## Created by: Qt User Interface Compiler version 6.6.0
+##
+## WARNING! All changes made in this file will be lost when recompiling UI file!
+################################################################################
+
+from PySide6.QtCore import (QCoreApplication, QDate, QDateTime, QLocale,
+    QMetaObject, QObject, QPoint, QRect,
+    QSize, QTime, QUrl, Qt)
+from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
+    QFont, QFontDatabase, QGradient, QIcon,
+    QImage, QKeySequence, QLinearGradient, QPainter,
+    QPalette, QPixmap, QRadialGradient, QTransform)
+from PySide6.QtWidgets import (QApplication, QHeaderView, QMainWindow, QMenuBar,
+    QPushButton, QSizePolicy, QStatusBar, QTableWidget,
+    QTableWidgetItem, QWidget)
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        if not MainWindow.objectName():
+            MainWindow.setObjectName(u"MainWindow")
+        MainWindow.resize(800, 600)
+        self.centralwidget = QWidget(MainWindow)
+        self.centralwidget.setObjectName(u"centralwidget")
+        self.pb_open = QPushButton(self.centralwidget)
+        self.pb_open.setObjectName(u"pb_open")
+        self.pb_open.setGeometry(QRect(50, 20, 141, 23))
+        self.pb_new_event = QPushButton(self.centralwidget)
+        self.pb_new_event.setObjectName(u"pb_new_event")
+        self.pb_new_event.setGeometry(QRect(450, 20, 111, 23))
+        self.pb_fertig = QPushButton(self.centralwidget)
+        self.pb_fertig.setObjectName(u"pb_fertig")
+        self.pb_fertig.setEnabled(True)
+        self.pb_fertig.setGeometry(QRect(630, 20, 80, 23))
+        self.tableWidget = QTableWidget(self.centralwidget)
+        if (self.tableWidget.columnCount() < 7):
+            self.tableWidget.setColumnCount(7)
+        __qtablewidgetitem = QTableWidgetItem()
+        self.tableWidget.setHorizontalHeaderItem(0, __qtablewidgetitem)
+        __qtablewidgetitem1 = QTableWidgetItem()
+        self.tableWidget.setHorizontalHeaderItem(1, __qtablewidgetitem1)
+        __qtablewidgetitem2 = QTableWidgetItem()
+        self.tableWidget.setHorizontalHeaderItem(2, __qtablewidgetitem2)
+        __qtablewidgetitem3 = QTableWidgetItem()
+        self.tableWidget.setHorizontalHeaderItem(3, __qtablewidgetitem3)
+        __qtablewidgetitem4 = QTableWidgetItem()
+        self.tableWidget.setHorizontalHeaderItem(4, __qtablewidgetitem4)
+        __qtablewidgetitem5 = QTableWidgetItem()
+        self.tableWidget.setHorizontalHeaderItem(5, __qtablewidgetitem5)
+        __qtablewidgetitem6 = QTableWidgetItem()
+        self.tableWidget.setHorizontalHeaderItem(6, __qtablewidgetitem6)
+        self.tableWidget.setObjectName(u"tableWidget")
+        self.tableWidget.setGeometry(QRect(30, 70, 741, 441))
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QMenuBar(MainWindow)
+        self.menubar.setObjectName(u"menubar")
+        self.menubar.setGeometry(QRect(0, 0, 800, 20))
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QStatusBar(MainWindow)
+        self.statusbar.setObjectName(u"statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+
+        QMetaObject.connectSlotsByName(MainWindow)
+    # setupUi
+
+    def retranslateUi(self, MainWindow):
+        MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"MainWindow", None))
+        self.pb_open.setText(QCoreApplication.translate("MainWindow", u"Kalenderdatei einlesen", None))
+        self.pb_new_event.setText(QCoreApplication.translate("MainWindow", u"termin erstellen", None))
+        self.pb_fertig.setText(QCoreApplication.translate("MainWindow", u"fertig", None))
+        ___qtablewidgetitem = self.tableWidget.horizontalHeaderItem(0)
+        ___qtablewidgetitem.setText(QCoreApplication.translate("MainWindow", u"Starttag", None));
+        ___qtablewidgetitem1 = self.tableWidget.horizontalHeaderItem(1)
+        ___qtablewidgetitem1.setText(QCoreApplication.translate("MainWindow", u"Startzeit", None));
+        ___qtablewidgetitem2 = self.tableWidget.horizontalHeaderItem(2)
+        ___qtablewidgetitem2.setText(QCoreApplication.translate("MainWindow", u"Endtag", None));
+        ___qtablewidgetitem3 = self.tableWidget.horizontalHeaderItem(3)
+        ___qtablewidgetitem3.setText(QCoreApplication.translate("MainWindow", u"EndZeit", None));
+        ___qtablewidgetitem4 = self.tableWidget.horizontalHeaderItem(4)
+        ___qtablewidgetitem4.setText(QCoreApplication.translate("MainWindow", u"Ort", None));
+        ___qtablewidgetitem5 = self.tableWidget.horizontalHeaderItem(5)
+        ___qtablewidgetitem5.setText(QCoreApplication.translate("MainWindow", u"Titel", None));
+        ___qtablewidgetitem6 = self.tableWidget.horizontalHeaderItem(6)
+        ___qtablewidgetitem6.setText(QCoreApplication.translate("MainWindow", u"Beschreibung", None));
+    # retranslateUi
+
